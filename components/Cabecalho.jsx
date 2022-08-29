78b71c2641d68323f6eb2ff836c67e7a4af4d60c
Lucas Mota <contato.lukamota@gmail.com>
2022-08-29 08:08:40 -0300

    feat: change on components

--- components/Cabecalho.jsx
+++ components/Cabecalho.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function Cabecalho() {
 	return (
 		<>
-			<section className="bg-white dark:bg-gray-900">
+			{/* <section className="bg-white dark:bg-gray-900">
 				<div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
 					<div className="mr-auto place-self-center lg:col-span-7">
 						<h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
@@ -18,18 +18,6 @@ export function Cabecalho() {
 							className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
 						>
 							Confira Nossos Serviços
-							{/* <svg
-								className="w-5 h-5 ml-2 -mr-1"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fillRule="evenodd"
-									d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-									clipRule="evenodd"
-								/>
-							</svg> */}
 						</a>
 						<a
 							href="#"
@@ -39,7 +27,47 @@ export function Cabecalho() {
 						</a>
 					</div>
 					<div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-						<Image src="/welcome.jpg" alt="mockup" width={550} height={500} />
+						<Image
+							src="/atendente.jpg"
+							alt="mockup"
+							width={1000}
+							height={800}
+						/>
+					</div>
+				</div>
+			</section> */}
+
+			<section className="text-gray-600 body-font">
+				<div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+					<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+						<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+							Before they sold out
+							<br className="hidden lg:inline-block" />
+							readymade gluten
+						</h1>
+						<p className="mb-8 leading-relaxed">
+							Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+							air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+							park mlkshk tote bag selvage hot chicken authentic tumeric
+							truffaut hexagon try-hard chambray.
+						</p>
+						<div className="flex justify-center">
+							<button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+								Confira Nossos Serviços
+							</button>
+							<button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+								Sobre o Cartório
+							</button>
+						</div>
+					</div>
+					<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+						<Image
+							className="object-cover object-center rounded"
+							src="/atendente.jpg"
+							alt="mockup"
+							width={700}
+							height={600}
+						/>
 					</div>
 				</div>
 			</section>
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -4,30 +4,32 @@ import { Navbar } from 'flowbite-react'
 export function Menu() {
 	return (
 		<>
-			<Navbar fluid={true} rounded={true}>
-				<Navbar.Brand href="/">
-					<Image
-						src="/1.png"
-						width={60}
-						height={80}
-						className="mr-3 h-6 sm:h-9"
-						alt="Logo do Cartório De Registro De Imóveis, Hipotecas, Títulos e Documentos - São Gonçalo Dos Campos"
-					/>
-					<span className="self-center whitespace-nowrap ml-2 text-xl font-semibold dark:text-white">
-						Cartório RI RTDPJ
-					</span>
-				</Navbar.Brand>
-				<Navbar.Toggle />
-				<Navbar.Collapse>
-					<Navbar.Link href="/navbars" active={true}>
-						Início
-					</Navbar.Link>
-					<Navbar.Link href="/navbars">Serviços</Navbar.Link>
-					<Navbar.Link href="/navbars">Sobre</Navbar.Link>
-					<Navbar.Link href="/navbars">Modelos</Navbar.Link>
-					<Navbar.Link href="/navbars">Contato</Navbar.Link>
-				</Navbar.Collapse>
-			</Navbar>
+			<div className="container mx-auto">
+				<Navbar fluid={true} rounded={true}>
+					<Navbar.Brand href="/">
+						<Image
+							src="/1.png"
+							width={40}
+							height={60}
+							className="mr-3 h-6 sm:h-9"
+							alt="Logo do Cartório De Registro De Imóveis, Hipotecas, Títulos e Documentos - São Gonçalo Dos Campos"
+						/>
+						<span className="self-center whitespace-nowrap ml-2 text-xl font-semibold dark:text-white">
+							Cartório RI RTDPJ
+						</span>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+					<Navbar.Collapse>
+						<Navbar.Link href="/navbars" active={true}>
+							Início
+						</Navbar.Link>
+						<Navbar.Link href="/navbars">Serviços</Navbar.Link>
+						<Navbar.Link href="/navbars">Sobre</Navbar.Link>
+						<Navbar.Link href="/navbars">Modelos</Navbar.Link>
+						<Navbar.Link href="/navbars">Contato</Navbar.Link>
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
 		</>
 	)
 }
