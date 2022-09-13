9cb4f45a0728527a40846b3f25aa428fa84fd076
Lucas Mota <contato.lukamota@gmail.com>
2022-09-13 18:33:02 -0300

    chore: update the contact form

--- components/Contato.jsx
+++ components/Contato.jsx
@@ -9,7 +9,18 @@ export function Contato() {
 					<p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
 						Tem alguma dúvida? Entre em contato conosco!
 					</p>
-					<form action="#" className="space-y-8">
+					<form
+						name="contact"
+						method="POST"
+						action="#"
+						netlify-honeypot="bot-field"
+						data-netlify="true"
+						className="space-y-8"
+					>
+						<label className="invisible">
+							Não preencha se você não for humano: <input name="bot-field" />
+						</label>
+						<input type="hidden" name="form-name" value="contact" />
 						<div>
 							<label
 								htmlFor="yourname"
@@ -22,7 +33,7 @@ export function Contato() {
 								id="yourname"
 								className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 								placeholder="Informe o seu nome"
-								required=""
+								required={true}
 							/>
 						</div>
 						<div>
@@ -37,7 +48,7 @@ export function Contato() {
 								id="email"
 								className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 								placeholder="Informe o seu e-mail"
-								required=""
+								required={true}
 							/>
 						</div>
 						<div className="sm:col-span-2">
