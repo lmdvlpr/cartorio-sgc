e3ea4899287d252ecd24431ab57e77f8113fdf50
Lucas Mota <contato.lukamota@gmail.com>
2022-09-13 19:02:28 -0300

    chore: add new features to the form contact

--- components/Contato.jsx
+++ components/Contato.jsx
@@ -1,4 +1,14 @@
+import { useState, useEffect } from 'react'
+
 export function Contato() {
+	const [success, setSuccess] = useState(false)
+
+	useEffect(() => {
+		if (window.location.search.includes('success=true')) {
+			setSuccess(true)
+		}
+	}, [])
+
 	return (
 		<>
 			<section className="bg-white dark:bg-gray-900" id="contato">
@@ -9,10 +19,15 @@ export function Contato() {
 					<p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
 						Tem alguma dúvida? Entre em contato conosco!
 					</p>
+					{success && (
+						<p className="mb-8 lg:mb-16 font-light text-center text-green-500 dark:text-green-400 sm:text-xl">
+							Sua mensagem foi enviada com sucesso!
+						</p>
+					)}
 					<form
 						name="contact"
 						method="POST"
-						action="#"
+						action="/?success=true"
 						netlify-honeypot="bot-field"
 						data-netlify="true"
 						className="space-y-8"
@@ -20,7 +35,7 @@ export function Contato() {
 						<label className="invisible">
 							Não preencha se você não for humano: <input name="bot-field" />
 						</label>
-						<input type="hidden" name="form-name" value="contact" />
+						<input type="hidden" name="form-name" value="Contato via Site" />
 						<div>
 							<label
 								htmlFor="yourname"
@@ -30,10 +45,11 @@ export function Contato() {
 							</label>
 							<input
 								type="text"
+								name="yourname"
 								id="yourname"
 								className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 								placeholder="Informe o seu nome"
-								required={true}
+								required
 							/>
 						</div>
 						<div>
@@ -45,10 +61,11 @@ export function Contato() {
 							</label>
 							<input
 								type="email"
+								name="email"
 								id="email"
 								className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 								placeholder="Informe o seu e-mail"
-								required={true}
+								required
 							/>
 						</div>
 						<div className="sm:col-span-2">
@@ -59,11 +76,13 @@ export function Contato() {
 								Mensagem
 							</label>
 							<textarea
+								name="message"
 								id="message"
 								rows={6}
 								className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 								placeholder="Escreva aqui a sua mensagem..."
 								defaultValue={''}
+								required
 							/>
 						</div>
 						<button
